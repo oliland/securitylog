6591b7760a3e1f90cb442b9f5dfa562c83460990
Oli Kingshott <oli@oliland.net>
2013-02-08 12:30:55 +0000

    fucking jsonp

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
     function parseRSS(url, callback) {
       $.ajax({
         url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-        dataType: 'xml',
-        crossDomain: true,
+        dataType: 'jsonp',
         success: function(data) {
           callback(data.responseData.feed);
         }
